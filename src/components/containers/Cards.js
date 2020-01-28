a2b3145efe9a0502403ef57ca20d8ed06aaa7e89
Emily Fang <fangemily60@gmail.com>
2020-01-28 10:10:53 -0500

    I am going to start making a cards/notes application to focus on learning backend

---
+++ src/components/containers/Cards.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react'
+
+class Cards extends Component {
+    constructor(props) {
+	super(props);
+	this.state = {
+
+	}
+    }
+
+    handleEdit = () => {
+
+    }
+
+    handleChange = (event) => {
+
+    }
+    
+    render() {
+	return ();
+    }
+}
+
+export default Cards;
